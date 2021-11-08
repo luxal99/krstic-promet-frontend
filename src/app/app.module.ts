93ef9f362806062917205c116ee47e09afa252fb
Aleksa Lukic <lukicaleksa04@gmail.com>
2021-11-08 14:02:04 +0100

    Setup util package

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from "@angular/core";
+import {BrowserModule} from "@angular/platform-browser";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from "./app-routing.module";
+import {AppComponent} from "./app.component";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {MaterialModule} from "./material.module";
+import {FormModule} from "./util/form-components/form.module";
+import {DEFAULT_ROUTE} from "./constant/constant";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        MaterialModule,
+        BrowserAnimationsModule,
+        FormModule
+    ],
+    providers: [{provide: DEFAULT_ROUTE, useValue: ""}],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
